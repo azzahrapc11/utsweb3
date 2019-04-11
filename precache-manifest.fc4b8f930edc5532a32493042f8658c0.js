4a882925097470f69a9e9f95de123bc1a0c6dffa
azzahrapc11 <azzahrapramata@gmail.com>
2019-04-11 08:51:19 +0700

    Updates

---
+++ precache-manifest.fc4b8f930edc5532a32493042f8658c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "8680a41049506368e067",
+    "url": "/utsweb3/static/js/main.44df580c.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/utsweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8680a41049506368e067",
+    "url": "/utsweb3/static/css/main.4d56d0d5.chunk.css"
+  },
+  {
+    "revision": "f46bc376e2a1b90891613b0a17323659",
+    "url": "/utsweb3/index.html"
+  }
+];
